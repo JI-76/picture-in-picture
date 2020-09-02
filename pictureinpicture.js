993907a1e434a1d6a59dcf1e582cf862b5436cfa
JI-76 <johnison76@hotmail.com>
2020-09-02 13:19:04 -0700

    Functionality Javascript

--- pictureinpicture.js
+++ pictureinpicture.js
@@ -1,2 +1,34 @@
 // pictureinpicture.js
 
+// Constants for HTML elements
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Asynch Function - prompt user to select media stream, pass to Video element.
+async function selectMediaStream() {
+    try {
+        // constant for media stream content
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        // once media stream content is selected use it as a source for our Video Element <video>     
+        videoElement.srcObject = mediaStream;
+        // once media stream content is loaded; play
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        // Catch error
+        console.log('Error:', error);
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable the button after being clicked
+    button.disabled = true;
+    // Start Picture In Picture
+    await videoElement.requestPictureInPicture();
+    // Reset the button
+    button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
